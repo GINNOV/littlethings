03d88d80bef57f393ccc32b31d226ac51bea36e7
ME <ve@theblifemovement.com>
2025-07-17 19:28:55 +0200

    fixed a bunch of xcode settings

---
+++ Amiga/Tools/AuDeluxe/AuDeluxeQL/AuDeluxeQL-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  AuDeluxe-Bridging-Header.h
+//  AuDeluxe
+//
+//  Created by Mario Esposito on 7/17/25.
+//
+
+#ifndef AuDeluxe_Bridging_Header_h
+#define AuDeluxe_Bridging_Header_h
+#import <libopenmpt/libopenmpt.h>
+
+#endif /* AuDeluxe_Bridging_Header_h */
