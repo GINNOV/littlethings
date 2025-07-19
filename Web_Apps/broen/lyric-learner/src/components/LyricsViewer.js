b03ae65067aea5d9839a516ec6e4db52bdbe8afc
ME <ve@theblifemovement.com>
2025-07-19 18:58:12 +0200

    added build version in the footer and trying again with CORS pain problem for the youtube video

--- Web_Apps/broen/lyric-learner/src/components/LyricsViewer.js
+++ Web_Apps/broen/lyric-learner/src/components/LyricsViewer.js
@@ -6,10 +6,15 @@ import WordComponent from './WordComponent';
 
 const LyricsViewer = ({ songData, activeLayer, onWordClick, layers, showTooltipSetting, showItalianSetting, activeWordIndex }) => {
   let wordCounter = -1;
-  
+  const youtubeLink = "https://www.youtube.com/watch?v=9C1BCAgu2I8&list=RD9C1BCAgu2I8&start_radio=1"; // Your provided link
+
   return (
     <div className="lyrics-container">
-      <h2 className="poem-title">{songData.title}</h2>
+      <h2 className="poem-title">
+        <a href={youtubeLink} target="_blank" rel="noopener noreferrer">
+            {songData.title}
+        </a>
+      </h2>
       <p className="poem-author">by {songData.author}</p>
       {songData.stanzas.map((stanza, stanzaIndex) => (
         <div className="stanza" key={stanzaIndex}>
