a0c10a8f1fcdd5ab69e5ea597a07b8a0fc21ac2f
Dunno Much <ve@theblifemovement.com>
2026-03-11 17:43:06 -1000

    clean up

--- Amiga/Tools/amigALM/sources/Arny's_Dreams/back.sh
+++
@@ -1,7 +0,0 @@
-cls
-format drive df1: name AD_Back quick
-cp work:Arny's_Dreams/.../*.s df1:
-cp work:Arny's_Dreams/.../*.gfa df1:
-rm work:Arny's_Dreams/.../*.info
-rm work:Arny's_Dreams/.../*.bak
-cls
